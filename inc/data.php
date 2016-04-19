c61b62076382957153496c164e394b4562a28677
papaponmx <jaimemadrigalrios@gmail.com>
2016-04-19 18:00:35 -0500

    Removed the accents

--- inc/data.php
+++ inc/data.php
@@ -60,25 +60,25 @@ $productos = [];
 $productos[201] = [
     "nombre" => "hidrobac",
     "img" => "http://lorempixel.com/400/200/",
-    "descripción" => "Ideal para aplicación en plantas de tratamiento de agua residual, fosas sépticas, lagunas de oxidación, coladeras, etc. Compuesto 100% natural de micronutrientes básicos acelerador para incremento de biomasa bacteriana. ¡Elimina malos olores y acelera la degradación de la materia orgánico!",
+    "descripcion" => "Ideal para aplicación en plantas de tratamiento de agua residual, fosas sépticas, lagunas de oxidación, coladeras, etc. Compuesto 100% natural de micronutrientes básicos acelerador para incremento de biomasa bacteriana. ¡Elimina malos olores y acelera la degradación de la materia orgánico!",
     "presentaciones" => "1lt, 2lt y 20lt"
 ];
 
 $productos[202] = [
     "nombre" => "hidrobac BP",
     "img" => "http://lorempixel.com/400/200/",
-    "descripción" => "Ideal para baños portátiles logra la descomposición de los desechos orgánicos y la eliminación de malos olores. Ideal para aplicación en baños portátiles, vehículos recreativos etc.Compuesto 100% natural de micronutrientes básicos acelerador para incremento de biomasa bacteriana.",
+    "descripcion" => "Ideal para baños portátiles logra la descomposición de los desechos orgánicos y la eliminación de malos olores. Ideal para aplicación en baños portátiles, vehículos recreativos etc.Compuesto 100% natural de micronutrientes básicos acelerador para incremento de biomasa bacteriana.",
     "presentaciones" => "1lt, 2lt y 20lt"
 ];
 $productos[203] = [
     "nombre" => "hidrobac QO",
     "img" => "http://lorempixel.com/400/200/",
-    "descripción" => "Control inmediato de olores de fácil aplicación, y segura para el medio ambiente, Hidrobac QO es ideal para aplicación en botes de basura, zapatos, pañales, areneros, entre otros. Compuesto 100% natural de micronutrientes básicos para el control de olores ocasionados por la descomposición de la materia orgánica.",
+    "descripcion" => "Control inmediato de olores de fácil aplicación, y segura para el medio ambiente, Hidrobac QO es ideal para aplicación en botes de basura, zapatos, pañales, areneros, entre otros. Compuesto 100% natural de micronutrientes básicos para el control de olores ocasionados por la descomposición de la materia orgánica.",
     "presentaciones" => "1lt, 2lt y 20lt"
 ];
 $productos[204] = [
     "nombre" => "Hidrobac TG",
     "img" => "http://lorempixel.com/400/200/",
-    "descripción" => "Ideal para trampas de grasa, Hidrobac TG es un compuesto 100% natural de micronutrientes básicos para incremento de biomasa bacteriana, acelera la descomposición de los sólidos, aceites, grasas y por ende elimina olores y mejora el efluente de las trampas de grasa. Los beneficios: control de olores, fácil aplicación, mejora la descarga a la red municipal, menor desazolve. Ideal para la industria alimenticia.",
+    "descripcion" => "Ideal para trampas de grasa, Hidrobac TG es un compuesto 100% natural de micronutrientes básicos para incremento de biomasa bacteriana, acelera la descomposición de los sólidos, aceites, grasas y por ende elimina olores y mejora el efluente de las trampas de grasa. Los beneficios: control de olores, fácil aplicación, mejora la descarga a la red municipal, menor desazolve. Ideal para la industria alimenticia.",
     "presentaciones" => "1lt, 2lt y 20lt"
 ];
