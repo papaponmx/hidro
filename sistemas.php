3cdb4cb514592de41de38e0bd2cd0a30cfb76700
papaponmx <jaimemadrigalrios@gmail.com>
2016-04-19 11:30:01 -0500

    added a grid with the content

--- sistemas.php
+++ sistemas.php
@@ -4,6 +4,58 @@ $section= 'sistemas';
 include ('inc/header.php');
 ?>
 
+<div class="sistemas-div">
+    <h1>Nuestros sistemas</h1>
+
+    <div class="col-sm-6 bio">
+     <h2>Sistemas Biológicos</h2>
+        Los sistemas de tratamiento de aguas residuales se emplean para lograr la remoción de contaminantes que son agregados al agua producto de diferentes <br>actividades. Las plantas de tratamiento con sistema QUÍMICO son utilizadas principalmente para el tratamiento de aguas jabonosas o para aguas de <br>tendencia industrial tales como AGUAS CONGENITAS, RESIDUOS DE LA INDUSTRIA QUÍMICA O ALTAS CARGAS ORGÁNICAS EN DBO5, donde los contaminantes del algua varían.<br><br>
+
+        HIDROBIOTECNOLOGIA te ofrece la mejor tecnológía así como sistemas especilaizados y diseñados especificamente para satisfacer tus necesidades, dependiendo <br>del tipo de agua residual, el flujo a tratar y el rehuso que quieras darle al agua tratada o la norma con la que deseas cumplir.
+        <br><br>
+        Una vez que el agua ha sido tratada puede ser rehusada para diferentes actividades tales como, riego de áreas verdes, descarga en sanitarios, lavado de <br>autos, lavado de vidrios y ventanas, vertida a cuerpos de agua (lagos, ríos, laguna, etc), dependiendo de la norma que se cumpla al final del tratamiento.
+        <br><br>
+        Te ofrecemos el mejor costo por m3 tratado, somos una empresa con tecnología 100% mexicana, solo nosotros te ofrecemos el mejor precio.
+        <br><br>
+        <strong>¡SOMOS FABRICANTES!</strong>
+    </div>
+
+    <div class="col-sm-6 quimicos">
+        <h2>Sistemas Químicos</h2>
+        <p>El Sistema Mixto-Biológico implimente un proceso anaerobio que consisten en poner en contacto el agua residual con un cultivo<br> de microorganismos en ausencia de aire dentro de un recipiente cerrado.<br><br>
+            Este sistema de tratamiento es un proceso biológico de contacto, en el que los microorganismos y los sólidos orgánicos de las aguas residuales se mezclan íntimamente en un medio favorable para la descomposición aerobia de estos últimos.<br><br>
+            El sistema está integrado por un Reactor Anaerobio  en combinación con un Filtro Percolador aireado empacado<br>
+
+            de flujo descendente; en cuanto a la desinfección del agua residual, se utiliza un tanque de desinfección por Ozono, este<br> sistema es altamente eficiente ya que actúa 3,000 veces más que el cloro y no deja trazas.  El ozono se produce por alto <br>
+            voltaje y fuente corona y el único consumible que se necesita para producirlo es oxígeno  y  lo tenemos en el medio ambiente.<br><br>
+
+            Este sistema proporciona una eficiencia de las aguas residuales de un 90-95% cumpliendo así con las normas:</p><br>
+            <ul>
+                <strong>
+                    <li>NOM-SEMARNAT-01-96</li>
+                    <li>NOM-SEMARNAT-02-96</li>
+                    <li>NOM-SEMARNAT-03-97</li>
+                    <li>NOM-SEMARNAT-04-02.</li>
+                </strong>
+            </ul>
+
+    </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php include ('inc/footer.php'); ?>
