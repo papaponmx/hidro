9d5c859e0993c995c58fe52119693f93ec144740
papaponmx <jaimemadrigalrios@gmail.com>
2016-04-19 22:20:19 -0500

    A loop for $productos[] has been added.

--- productos.php
+++ productos.php
@@ -1,21 +1,33 @@
 <?php
-$pageTitle = 'Nosotros' ;
+include ('inc/data.php');
+include ('inc/functions.php');
+
+$pageTitle = 'Productos' ;
 $section = 'productos';
 include ('inc/header.php');
+
 ?>
-<div class="productos-div">
-    <h1>Conoce nuestros productos</h1>
 
 
+    <div class="productos-div">
+
+        <h1>Conoce nuestros productos</h1>
+
+         <?php
+            foreach ($productos as $producto) {
+                echo "<div class='producto'><h1>"
+                    . $producto["nombre"]
+                    . "</h1><p>"
+                    . $producto["descripcion"]
+                    . "</p><img src='"
+                    . $producto["img"]
+                    ."'></img></div>";
+            }
+         
+         
+         ?>
 
 
-
-
-
-</div>
-
-
-
-
+    </div>
 
 <?php include ('inc/footer.php'); ?>
