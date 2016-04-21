31a2f715d3b61e26b90a8344f14d03c0dd89bd65
papaponmx <jaimemadrigalrios@gmail.com>
2016-04-20 22:41:10 -0500

    Added a grid

--- productos.php
+++ productos.php
@@ -9,18 +9,18 @@ include ('inc/header.php');
 
 
     <div class="productos-div">
-
-        <h1>Conoce nuestros productos</h1>
+<!--
+        <h2>Conoce nuestros productos</h2>-->
 
          <?php
             foreach ($productos as $producto) {
-                echo "<div class='producto'><h1>"
+                echo "<div class='producto container'><h1>"
                     . $producto["nombre"]
-                    . "</h1><p>"
+                    . "</h1><div class='col-xs-6'><p>"
                     . $producto["descripcion"]
-                    . "</p><img src='"
+                    . "</p></div><div class='col-xs-6'><img src='"
                     . $producto["img"]
-                    ."'></img></div>";
+                    ."'></img></div></div>";
             }
          
          
