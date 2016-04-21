5c0e5fd99728ec757e144be41de7f85c00220494
papaponmx <jaimemadrigalrios@gmail.com>
2016-04-20 19:08:26 -0500

    Added some other grid classes

--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function get_item_html($id,$objeto){
-    $output = "<div class='col-xs-6 col-sm-3'><a href='detalles.php?id="
+    $output = "<div class='col-xs-6 col-sm-3 galeria-elemento'><a href='detalles.php?id="
         .$id."'><p>"
         . $objeto["titulo"]
         ."</p><img alt='lorem ipsum' src='"
