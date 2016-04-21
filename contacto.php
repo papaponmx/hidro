0ae890205d112c581839fca00bcc2d8d6534879f
papaponmx <jaimemadrigalrios@gmail.com>
2016-04-21 13:55:52 -0500

    Ready for dist

--- contacto.php
+++ contacto.php
@@ -4,66 +4,30 @@ $section = 'contacto';
 include ('inc/header.php');
 ?>
 
+<div class="home_cover contacto_div">
 
-<div class="contacto">
-    <h1>Contacto</h1>
 
-    <div class="container">
+<!--  Aquí va el código de www.foxyform.com-->
 
-        <form class="well form-horizontal" action="process.php" method="post"  id="contact_form">
-            <fieldset>
 
-                <!-- Text input-->
 
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="nombre">Nombre</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input  name="nombre" placeholder="Nombre" id="nombre" class="form-control"  type="text">
-                        </div>
-                    </div>
-                </div>
 
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="email">E-Mail</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                            <input name="email" placeholder="E-Mail" class="form-control" id="email" type="text">
-                        </div>
-                    </div>
-                </div>
 
-                <!-- Text area -->
-
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="mensaje">Mensaje</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
-                            <textarea class="form-control" name="mensaje" id="mensaje" placeholder="Tu mensaje aquí"></textarea>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Success message -->
-                <div class="alert alert-success" role="alert" id="success_message">Success <i class="glyphicon glyphicon-thumbs-up"></i> Gracias por contactarnos</div>
-
-                <!-- Button -->
-                <div class="form-group">
-                    <label class="col-md-4 control-label"></label>
-                    <div class="col-md-4">
-                        <button type="submit" class="btn btn-warning" >Send <span class="glyphicon glyphicon-send"></span></button>
-                    </div>
-                </div>
-
-            </fieldset>
-        </form>
-    </div>
-</div><!-- /.container -->
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
